64be23974aec2210f1b4be7a4471bee0b5fc5399
patrickeilert <patricklykkeeilert@Patricks-MacBook-Air.local>
2016-12-08 10:50:00 +0100

    Opdatering af struktur

---
+++ nav.php
@@ -0,0 +1,22 @@
+
+<div class="navbar-fixed">
+    <nav role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">
+                <img class="responsive-img" src="img/logo_text_64px.png">
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#home">OM</a></li>
+                <li><a href="#">OPRET</a></li>
+                <li><a href="#">LOG IND</a></li>
+            </ul>
+
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="#">Navbar Link</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse">
+                <i class="material-icons">menu</i>
+            </a>
+        </div>
+      </nav>
+  </div>
