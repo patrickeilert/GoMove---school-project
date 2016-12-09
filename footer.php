6ebba8f04486f6bb13f60ba1349400c156cb6e16
patrickeilert <patricklykkeeilert@Patricks-Air.lan>
2016-12-09 08:44:00 +0100

    Footer flyttet til .php (include)

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="page-footer orange">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">GoMove</h5>
+                        <p class="grey-text text-lighten-4">
+                        <ul>
+                            <li><a class="white-text" href="#!">Nørgaardsvej 30</a></li>
+                            <li><a class="white-text" href="#!">2800 Kgs. Lyngby</a></li>
+                            <li><a class="white-text" href="#!">+45 44 68 10 00</a></li>
+                            <li><a class="white-text" href="#!">kontakt@gomove.dk</a></li>
+                        </ul>
+                        </p>
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text"></h5>
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text"></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container center">
+                    Skoleprojekt af <a class="orange-text text-lighten-3" href="http://materializecss.com">Caroline, Patricia & Patrick</a>
+                </div>
+            </div>
+        </footer>
--- index.php
+++ index.php
@@ -69,35 +69,7 @@
                 </div>
             </div>
         </div>
-  
-        <footer class="page-footer orange">
-            <div class="container">
-                <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="white-text">GoMove</h5>
-                        <p class="grey-text text-lighten-4">
-                        <ul>
-                            <li><a class="white-text" href="#!">Nørgaardsvej 30</a></li>
-                            <li><a class="white-text" href="#!">2800 Kgs. Lyngby</a></li>
-                            <li><a class="white-text" href="#!">+45 44 68 10 00</a></li>
-                            <li><a class="white-text" href="#!">kontakt@gomove.dk</a></li>
-                        </ul>
-                        </p>
-                    </div>
-                    <div class="col l3 s12">
-                        <h5 class="white-text"></h5>
-                    </div>
-                    <div class="col l3 s12">
-                        <h5 class="white-text"></h5>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-copyright">
-                <div class="container center">
-                    Skoleprojekt af <a class="orange-text text-lighten-3" href="http://materializecss.com">Caroline, Patricia & Patrick</a>
-                </div>
-            </div>
-        </footer>
+        <?php include 'footer.php'; ?>
         <!--  Scripts-->
         <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="js/materialize.js"></script>
