c6ccf0abc4a248c8379311dd78a6b9f8e4453002
Patricia Gambula <patricia.gambula@gmail.com>
2016-12-14 11:00:58 +0100

    seo
    so tags

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+        <meta name="description" content="Er dine venner også træt af at bære kasser ned for 4. sal, mangler du penge til weekendens bytur - Hos GoMove hjælper vi hinanden nemt og billigt">
+<meta name="keywords" content="move, nem, billig">
         <title>GoMove - din flytteportal</title>
         <!-- CSS  -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -18,7 +20,7 @@
                     <!-- <h5 class="header col s12 light">Flyt dig Kaj - jeg vil gerne hjælpe</h5> -->
                 </div>
                 <div class="row center">
-                    <img class="responsive-img" src="img/logo_cirkel.png" width="40%">
+                    <img class="responsive-img" src="img/logo_cirkel.png" alt="GoMove billig og nem flytning" width="40%">
                 </div>
                 <div class="row center">
                     <a href="#" class="waves-effect waves-light btn-large" id="button">Jeg hjælper!</a>
@@ -34,7 +36,7 @@
                 <div class="row">
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <img class="responsive-img" src="img/om-grafik-18.png">
+                            <img class="responsive-img" src="img/om-grafik-18.png" alt="GoMove er en tjeneste hvor du har mulighed for at søge hjælp til flytning eller tilbyde hjælp">
                             <h5 class="center">Hvad</h5>
                             <p class="light">GoMove er en tjeneste, hvor du kan oprette dig som bruger. Som bruger har du mulighed for enten at søge hjælp til flytning, eller tilbyde andre hjælp. Har du en bil? Er du stærk? Har du kørekort? Har du en støvet trailer stående i garagen? Du kan tilbyde de kompetencer, du besidder, og dermed matche med den bruger, der søger dét du har.
                             </p>
@@ -42,7 +44,7 @@
                     </div>
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <img class="responsive-img" src="img/om-grafik-19.png">
+                            <img class="responsive-img" src="img/om-grafik-19.png" alt="Er dine venner også træt af at bære kasser ned for 4. sal, mangler du penge til weekendens bytur - Hos GoMove hjælper vi hinanden nemt og billigt">
                             <h5 class="center">Hvorfor</h5>
                             <p class="light">GoMove er et nyt tiltag for unge og studerende i København, som flytter meget rundt i deres studietid. 3 måneder fremleje her, 6 måneder på et værelse dér, bliver hurtigt til mange flytninger på et år. Som ung på SU er det dyrt at benytte sig af flyttefirmaer, og er derfor ikke altid en mulighed. Her kommer GoMove ind i billedet!
                             </p>
@@ -50,7 +52,7 @@
                     </div>
                     <div class="col s12 m4">
                         <div class="icon-block">
-                            <img class="responsive-img" src="img/om-grafik-20.png">
+                            <img class="responsive-img" src="img/om-grafik-20.png" alt="Opret dig som bruger og flyt elelr hjælp allerede i dag">
                             <h5 class="center">Hvordan</h5>
                             <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.
                             </p>
@@ -66,7 +68,7 @@
                     <h5 class="header col s12 light">Find flytninger og hjælp nær dig</h5>
                 </div>
                 <div class="row center">
-                    <img class="responsive-img" src="img/map.png">
+                    <img class="responsive-img" src="img/map.png" alt="Se flytninger og hjælpende hænder nær dig">
                 </div>
             </div>
         </div>
