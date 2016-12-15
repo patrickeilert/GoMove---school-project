09181053e4249946dfab33344b295728dc9a3e9d
patrickeilert <patricklykkeeilert@Patricks-MacBook-Air.local>
2016-12-15 10:19:31 +0100

    login virker part1

--- logon.php
+++ logon.php
@@ -1,3 +1,35 @@
+<?php 
+
+session_start();
+
+if( isset($_SESSION['user_id']) ){
+    header("Location: index.php");
+}
+
+require 'dbcon.php';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])):
+
+    $records = $conn->prepare('SELECT user_id, email, password FROM user WHERE email = :email');
+    $records->bindParam(':email', $_POST['email']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $message = '';
+
+    if(count($results) > 0 && password_verify($_POST['password'], $results['password'])){
+        
+        $_SESSION['user_id'] = $results['id'];
+        header("Location: index.php");
+        
+    } else {
+        
+        $message = 'Sorry passer ikke';
+    }
+
+endif;
+?>
+
 <!DOCTYPE html>
 <html lang="da">
     <head>
@@ -13,31 +45,53 @@
     </head>
     <body>
         <?php include 'nav.php'; ?>
+        
+         <?php if(!empty($message)): ?>
+        <p><?= $message ?></p>
+    <?php endif; ?>
+    <!-- 
+    <h1>Login</h1>
+    <span>or <a href="register.php">register here</a></span>
+     
+     <form action="login.php" method="post">
+        
+            <input type="text" placeholder="Enter your email" name="email">
+            <input type="password" placeholder="and password" name="password">
+            
+            <input type="submit">
+         
+         
+     </form>
+    -->   
+        
         <div class="section" id="login">
             <div class="container">
                 <h1 class="header center">Log ind</h1>
                 <div class="row">
-                    <form action="#" class="col s12">
+                    <form action="logon.php" method="post" class="col s12">
                         <div class="row">
                             <div class="col s4"></div>
                             <div class="input-field col s4">
-                                <input id="name" type="text" class="validate">
-                                <label for="name">Navn</label>
+                                <input id="email" type="text" name="email" class="validate">
+                                <label for="email" data-error="wrong" data-success="right">E-mail</label>
                             </div>
                             <div class="col s4"></div>
                             </div>
                             <div class="row">
                             <div class="col s4"></div>
                             <div class="input-field col s4">
-                                <input id="password" type="password" class="validate">
+                                <input id="password" type="password" name="password" class="validate">
                                 <label for="password">Password</label>
                             </div>
                             <div class="col s4"></div> 
                         </div>
                         <div class="row center">
+                            <input type="submit">   
+                            <!--
                             <a class="waves-effect waves-light btn" id="buttonLogon">Log ind</a>
                             <a class="waves-effect waves-light btn" id="buttonFB">Facebook</a>
-                            <a class="waves-effect waves-light btn" id="buttonG">Google+</a>   
+                            <a class="waves-effect waves-light btn" id="buttonG">Google+</a>
+                            -->   
                         </div>     
                     </form>
                 </div>
